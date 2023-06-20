f68859e1b341f9975a4e24a53fab53780fee97aa
jesmathes <132949201+jesmathes@users.noreply.github.com>
2023-06-20 07:55:36 -0400

    #U2_01
    Guess the Number Part 1
    
    Signed-off-by: jesmathes <132949201+jesmathes@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,30 +1,20 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
 
-function randomNumber(userGuess, numberToGuess) {
-  // console.log("Random Number", computersNumber);
+
+//  PART 1  #U2_01
+
+function randomNumber(userGuess, computersNumber) {
 
   if (userGuess < 1 || userGuess > 100) {
-    return "Try again with a number between 1 and 100";
-  }
-  if (userGuess < numberToGuess) {
-    return "Higher";
-  } else if (userGuess > numberToGuess) {
-    return "Lower";
-  } else {
-    return "That's it! The number was " + numberToGuess + "!!!";
-  }
+  return "Please input a number between 1 and 100";
+
+} else if (userGuess < computersNumber) {
+  return "Higher";
+
+} else if (userGuess > computersNumber) {
+  return "Lower";
+
+} else (userGuess === numberToGuess); {
+  return `The number was ${userGuess}!`;
+}
 }
 
-function startCompGuess(num) {
-  return "Is it " + num + "?";
-}
-
-function compGuess(reply) {
-  if (reply === "lower") {
-    return "Lower";
-  } else if (reply === "correct") {
-    return "Correct";
-  } else if (reply === "higher") {
-    return "Higher";
-  }
-}
